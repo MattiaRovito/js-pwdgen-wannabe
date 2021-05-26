d9c69275de74b0a4c8f96966503c3c953356be63
MattiaR <rovitomattia91@gmail.com>
2021-05-26 16:21:02 +0200

    add password

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var name = prompt ('Inserisci il tuo nome');
+// console.log(name);
+
+
+var surname = prompt ('Inserisci il tuo cognome');
+// console.log(surname);
+
+var color = prompt ('Inserisci il tuo colore preferito');
+// console.log(color);
+
+
+
+var password = name + surname + color + '21';
+// console.log(password);
+
+document.getElementById('password').innerHTML += password;
